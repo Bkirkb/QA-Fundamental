73fccaa0d3b43cb62b86c2ffd13767969758b0b3
Bkirkb <Brendan@qa-fundamental.europe-west2-c.c.gitprojects-296511.internal>
2020-12-18 10:48:23 +0000

    vm broken or something, transferring to new vm

--- Program/Application/routes.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField,IntegerField
-from wtforms.validators import DataRequired
-
-class Catch(FlaskForm):
-    ID = IntegerField('Random Int')
-    description = StringField('Description of Task', validators=[DataRequired()])
-    submit = SubmitField('Add Task')
