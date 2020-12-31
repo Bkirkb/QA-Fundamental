33cc046ca2d1e0c62188d963ee8074143624a3ad
Bkirkb <Brendan@fishingcompanion.europe-west2-c.c.gitprojects-296511.internal>
2020-12-31 23:43:03 +0000

    refactored to-do-list core functionality into a new CRUD app

--- Program/application/models.py
+++ Program/application/models.py
@@ -1,46 +1,19 @@
 from application import db
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
 
-
-
-
-class Users(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    username= db.Column(db.String(25), nullable=False, unique=True)
-    password = db.Column(db.String(15), nullable=False)
-    forename = db.Column(db.String(30), nullable=False)
-    surname = db.Column(db.String(30), nullable=False)
-    email = db.Column(db.String(100), nullable=False, unique=True)
-    gear = db.Column(db.String(255), nullable=True)
-    catches = db.relationship('Catchdiary', backref='totalcatches')
-    topcatches = db.relationship('Catchdiary', backref='totaltopcatches')
 
 
 class Fish(db.Model):
+    id = db.Column(db.Integer, primary_key=True,)
+    name = db.Column(db.String(35), nullable=False, unique=True)
+    minweight = db.Column(db.Integer, nullable=False)
+    maxweight = db.Column(db.Integer, nullable=False)
+    catches = db.relationship('Catches', backref="catches", lazy=True)
 
+class Catches(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    fishname = db.Column(db.String(50), nullable=False)
-    minweight = db.Column(db.Numeric, nullable=False)
-    maxweight = db.Column(db.Numeric, nullable=False)
-    avgweight = db.Column(db.Numeric, nullable=False)
-
-class Catchdiary(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    userid = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    fishname = db.Column(db.String(35), nullable=False)
     fishid = db.Column(db.Integer, db.ForeignKey('fish.id'), nullable=False)
-    fishweight = db.Column(db.Numeric, nullable=False)
-    timetk = db.Column(db.DateTime, nullable=False)
-    dateoc = db.Column(db.DateTime, nullable=False)
-    weather = db.Column(db.String(20), nullable=True)
-    catchdesc = db.Column(db.String(100), nullable=True)
-    topcatch = db.Column(db.Boolean, nullable=False)
-    totalcatches = db.Column(db.Integer, nullable=False)
-    totaltopcatches = db.Column(db.Integer, nullable=False)
-
-class Mostactive(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    userid = db.Column(db.Integer, db.ForeignKey('user.id', nullable=False))
-    catches = db.Column(db.Integer, db.ForeignKey('Catchdiary.totalcatches', nullable=False))
-    topcatches = db.Column(db.Integer, db.ForeignKey('Catchdiary.totaltopcatches', nullable=False))
+    fishweight = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(200), nullable=True)
