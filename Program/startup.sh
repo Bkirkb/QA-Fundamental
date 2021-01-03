bad1768919cd2299580202cbf97f17124c4914bf
Bkirkb <home@fishingcompanion2.europe-west2-c.c.gitprojects-296511.internal>
2021-01-03 17:00:39 +0000

    startup script changing

--- Program/startup.sh
+++ Program/startup.sh
@@ -4,4 +4,6 @@ sudo python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 python3 create.py
+sudo systemctl stop fundamental.service
+sudo systemctl daemon-reload
 pytest --cov=application tests/ --cov-report html
