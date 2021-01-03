e645ea481de6613fece8759569eae51c2b74dc0a
Bkirkb <home@fishingcompanion2.europe-west2-c.c.gitprojects-296511.internal>
2021-01-03 17:27:38 +0000

    changed startup file

--- Program/startup.sh
+++ Program/startup.sh
@@ -3,7 +3,6 @@ cd /opt/QA-Fundamental/Program
 sudo python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-python3 create.py
 sudo systemctl stop fundamental.service
 sudo systemctl daemon-reload
 pytest --cov=application tests/ --cov-report html
